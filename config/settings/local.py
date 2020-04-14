fe3472c44e37347fcba67e8d8c952fac12a569b5
Pavel Tanchev <dcopm999@gmail.com>
2020-04-14 18:20:57 +0500

    Up django version

--- config/settings/local.py
+++ config/settings/local.py
@@ -11,7 +11,7 @@ SECRET_KEY = env(
     default="d3cZmYRCijhwzkSZkJZ3ee9NpyaT554e9f3FyWFuxKuDmTERMfrySxWKcoY1mgjd",
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1", "*"]
 
 # CACHES
 # ------------------------------------------------------------------------------
